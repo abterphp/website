765e695330fbb3d6f9974ee3f726e2ebaaf2be6c
Peter Aba <peter@peteraba.com>
2019-08-26 11:27:39 +0200

    Refactor use cases

--- tests/Events/Listeners/DashboardBuilderTest.php
+++ tests/Events/Listeners/DashboardBuilderTest.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 
 namespace AbterPhp\Website\Events\Listeners;
 
-use AbterPhp\Framework\Authorization\CombinedAdapter;
 use AbterPhp\Framework\Dashboard\Dashboard;
-use AbterPhp\Framework\Events\AuthReady;
 use AbterPhp\Framework\Events\DashboardReady;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-
-use AbterPhp\Website\Authorization\PageCategoryProvider as AuthProvider;
 
 class DashboardBuilderTest extends TestCase
 {
