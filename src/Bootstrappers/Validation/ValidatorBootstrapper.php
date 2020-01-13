45be64102e4c624667ee5fd47299bae43ccf399e
Peter Aba <peter@peteraba.com>
2020-01-13 01:59:36 +0100

    Validation bootstrapper based on abterphp-admin validation boostrapper

--- src/Bootstrappers/Validation/ValidatorBootstrapper.php
+++ src/Bootstrappers/Validation/ValidatorBootstrapper.php
@@ -4,22 +4,14 @@ declare(strict_types=1);
 
 namespace AbterPhp\Website\Bootstrappers\Validation;
 
-use AbterPhp\Framework\Constant\Env;
-use AbterPhp\Framework\Validation\Rules\Base64;
-use AbterPhp\Framework\Validation\Rules\ExactlyOne;
-use AbterPhp\Framework\Validation\Rules\Url;
-use AbterPhp\Framework\Validation\Rules\Uuid;
+use AbterPhp\Admin\Bootstrappers\Validation\ValidatorBootstrapper as BaseBootstrapper;
+use AbterPhp\Admin\Validation\Factory\User;
+use AbterPhp\Admin\Validation\Factory\UserGroup;
 use AbterPhp\Website\Validation\Factory\Block;
 use AbterPhp\Website\Validation\Factory\BlockLayout;
 use AbterPhp\Website\Validation\Factory\Page;
 use AbterPhp\Website\Validation\Factory\PageCategory;
 use AbterPhp\Website\Validation\Factory\PageLayout;
-use InvalidArgumentException;
-use Opulence\Environments\Environment;
-use Opulence\Framework\Validation\Bootstrappers\ValidatorBootstrapper as BaseBootstrapper;
-use Opulence\Ioc\IContainer;
-use Opulence\Validation\Rules\Errors\ErrorTemplateRegistry;
-use Opulence\Validation\Rules\RuleExtensionRegistry;
 
 /**
  * Defines the validator bootstrapper
@@ -30,71 +22,12 @@ class ValidatorBootstrapper extends BaseBootstrapper
      * @var array
      */
     protected $validatorFactories = [
+        User::class,
+        UserGroup::class,
         BlockLayout::class,
         Block::class,
         PageLayout::class,
         PageCategory::class,
         Page::class,
     ];
-
-    /**
-     * @inheritdoc
-     */
-    public function getBindings(): array
-    {
-        $bindings = array_merge(
-            parent::getBindings(),
-            $this->validatorFactories
-        );
-
-        return $bindings;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function registerBindings(IContainer $container)
-    {
-        parent::registerBindings($container);
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.StaticAccess)
-     *
-     * Registers the error templates
-     *
-     * @param ErrorTemplateRegistry $errorTemplateRegistry The registry to register to
-     *
-     * @throws InvalidArgumentException Thrown if the config was invalid
-     */
-    protected function registerErrorTemplates(ErrorTemplateRegistry $errorTemplateRegistry)
-    {
-        global $abterModuleManager;
-
-        $lang = Environment::getVar(Env::DEFAULT_LANGUAGE);
-
-        foreach ($abterModuleManager->getResourcePaths() as $path) {
-            $fullPath = realpath(sprintf('%s/lang/%s/validation.php', $path, $lang));
-            if (!$fullPath || !is_file($fullPath)) {
-                continue;
-            }
-
-            $config = require $fullPath;
-
-            $errorTemplateRegistry->registerErrorTemplatesFromConfig($config);
-        }
-    }
-
-    /**
-     * Registers any custom rule extensions
-     *
-     * @param RuleExtensionRegistry $ruleExtensionRegistry The registry to register rules to
-     */
-    protected function registerRuleExtensions(RuleExtensionRegistry $ruleExtensionRegistry)
-    {
-        $ruleExtensionRegistry->registerRuleExtension(new ExactlyOne());
-        $ruleExtensionRegistry->registerRuleExtension(new Uuid());
-        $ruleExtensionRegistry->registerRuleExtension(new Base64());
-        $ruleExtensionRegistry->registerRuleExtension(new Url());
-    }
 }
